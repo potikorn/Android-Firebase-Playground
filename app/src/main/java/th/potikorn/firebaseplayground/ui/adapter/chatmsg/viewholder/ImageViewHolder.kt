f71545a4299a4505e140fdf2ab5648714ae96063
potikorn <potikornth@gmail.com>
2018-09-18 16:16:34 +0700

    [WIP] upload and show uploaded successful

---
+++ app/src/main/java/th/potikorn/firebaseplayground/ui/adapter/chatmsg/viewholder/ImageViewHolder.kt
@@ -0,0 +1,14 @@
+package th.potikorn.firebaseplayground.ui.adapter.chatmsg.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import th.potikorn.firebaseplayground.dao.MessagesDao
+
+class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun onBindData(messagesDao: MessagesDao){
+        itemView?.apply {
+
+        }
+    }
+}
